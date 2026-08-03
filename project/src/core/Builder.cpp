b7e11713a90405271639f7789152f049494e4f05
Eric Faure Dunand Bruschi <eric.faure-dunand-bruschi@epitech.eu>
2026-08-03 19:13:08 +0200

    [-] removed the possibility of switch the manager type in builder

--- project/src/core/Builder.cpp
+++ project/src/core/Builder.cpp
@@ -25,10 +25,10 @@ CoreBuilder& CoreBuilder::SetPort(std::uint16_t port) {
 }
 
 std::unique_ptr<IManager> CoreBuilder::BuildManager() const {
-    if (_mode == SERVER)
+    /*if (_mode == SERVER)
         return std::make_unique<ServerManager>(static_cast<int>(_port), _sceneFile);
     if (_mode == CLIENT)
-        return std::make_unique<ClientManager>(_ip, _port);
+        return std::make_unique<ClientManager>(_ip, _port);*/
     return std::make_unique<DefaultManager>();
 }
 
