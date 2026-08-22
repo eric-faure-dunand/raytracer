d695acacc1d75a3976066699876a1cb388aec565
Eric Faure Dunand Bruschi* <eric.faure-dunand-bruschi@epitech.eu>
2026-08-23 00:27:33 +0200

    [+] readded printer && Error in includes

---
+++ project/includes/Error/FileNotFound.hpp
@@ -0,0 +1,15 @@
+
+#ifndef FILE_NOT_FOUND_HPP
+    #define FILE_NOT_FOUND_HPP
+    #include <iostream>
+    #include "IError.hpp"
+
+class FileNotFound : public IError {
+    std::string _FileNotFound;
+public:
+    FileNotFound(std::string msg) : _FileNotFound(msg) {};
+    int code() const noexcept override {return 2;};
+    const char *what() const noexcept override {return _FileNotFound.c_str();};
+};
+
+#endif
